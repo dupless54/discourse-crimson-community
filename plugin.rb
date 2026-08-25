4a790cd414100dcc11eb234066b630bfa473f869
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-25 06:56:46 +0300

    FIX: load Turkish community settings for tr_TR (#1)

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 # name: discourse-crimson-community
-# about: Provides live online members and profile-visitor history for the senin.me Crimson Channels theme.
-# version: 1.0.2
+# about: senin.me Crimson Channels teması için canlı çevrimiçi üyeler ve profil ziyaretçisi geçmişi sağlar.
+# version: 1.0.3
 # authors: ErespawN
 # url: https://github.com/dupless54/discourse-crimson-community
 # required_version: 3.3.0
